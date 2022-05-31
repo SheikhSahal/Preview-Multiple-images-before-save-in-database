c0137ac4de98fd5f64597d6d7a81e3703c93fec4
Muhammad Sahal Qasim <sahal.qasim@premier.com.pk>
2022-05-31 16:39:50 +0500

    Preview Multiple before save in database

---
+++ image/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using image.Models;
+
+namespace image.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            
+            return View();
+        }
+
+        [HttpPost] 
+        public ActionResult Index(IEnumerable<HttpPostedFileBase> file , VMclass model)
+        {
+            //var path = "";
+            //if (file != null)
+            //{
+            //    if (file.ContentLength > 0)
+            //    {
+            //        if (Path.GetExtension(file.FileName).ToLower() == ".jpg"
+            //          || Path.GetExtension(file.FileName).ToLower() == ".png"
+            //          || Path.GetExtension(file.FileName).ToLower() == ".gif"
+            //          || Path.GetExtension(file.FileName).ToLower() == ".jpeg"
+            //          || Path.GetExtension(file.FileName).ToLower() == ".mp4")
+            //        {
+            //            if (Path.GetExtension(file.FileName).ToLower() == ".mp4" == true)
+            //            {
+            //                path = Path.Combine(Server.MapPath("~/Content/Videos"), file.FileName);
+            //            }
+            //            else
+            //            { 
+            //            path = Path.Combine(Server.MapPath("~/Content/Image"), file.FileName);
+            //            }
+            //            file.SaveAs(path);
+            //        }
+            //        MastplayEntities db = new MastplayEntities();
+            //        Movies_detail mdm = new Movies_detail();
+
+            //        mdm.image_path = file.FileName;
+            //        mdm.m_title_name = model.m_title_name;
+            //        db.Movies_detail.Add(mdm);
+            //        db.SaveChanges();
+            //    }
+            //}
+            return View();
+        }
+
+
+    }
+}
---
+++ image/Models/VMclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace image.Models
+{
+    public class VMclass
+    {
+        public int md_id { get; set; }
+        public string m_title_name { get; set; }
+        public string Category_id { get; set; }
+        public string Category_type_id { get; set; }
+        public string version_id { get; set; }
+        public string image_path { get; set; }
+        public Nullable<System.DateTime> year { get; set; }
+    }
+}
